3dd358f9625df37d83c7c639e6402da03b594bcb
hamidisaac <isaac.hamid@gmail.com>
2024-11-25 17:46:24 +0330

    Routing  Navigation

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <h2>Hello Next.js</h2>;
+};
+export default Home;
---
+++ src/pages/news/[id]/index.tsx
@@ -0,0 +1,8 @@
+import {useRouter} from 'next/router';
+
+const NewsItem = () => {
+  const router = useRouter();
+  const {id} = router.query;
+  return <h2>Hello from News {id}</h2>;
+};
+export default NewsItem;
